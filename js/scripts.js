996dec6b93681f2f31f805148af4b72886f19624
c-okeyo <chrisokello26@gmail.com>
2022-11-12 10:28:18 +0300

    portfolio descriptions added

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,8 @@ $(document).ready(function () {
   $("#design-icon").click(function () {
     //  $().removeClass();
     $("#design").show();
+    $("#design").on
+  
   });
   $("#dev-icon").click(function () {
     // $removeClass();
@@ -11,4 +13,11 @@ $(document).ready(function () {
     // $("#development").removeClass();
     $("#product-management").show();
   });
+
+  $(".#icon8").hover(function(){
+    $("#icon8").css("opacity", 0.5);
+    }, function(){
+    $("#work8").show();
+  });
 });
+
