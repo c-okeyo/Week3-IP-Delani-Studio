b5e26325b9a53bf2ece0115a8b9334204f923fc9
c-okeyo <chrisokello26@gmail.com>
2022-11-12 10:45:49 +0300

    js script modified

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(document).ready(function () {
   $("#design-icon").click(function () {
-    //  $().removeClass();
     $("#design").show();
-    $("#design").on
-  
   });
   $("#dev-icon").click(function () {
-    // $removeClass();
     $("#development").show();
   });
   $("#product-icon").click(function () {
-    // $("#development").removeClass();
     $("#product-management").show();
   });
-
 });
-
