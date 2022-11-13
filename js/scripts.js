d13957f63283dc0ce9818e51c7a2aec86a63c25b
c-okeyo <chrisokello26@gmail.com>
2022-11-13 20:06:25 +0300

    Portfolio title display functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -15,3 +15,30 @@ $(document).ready(function () {
     $("#design").css("display", "none");
   });
 });
+
+$(document).ready(function () {
+  $("#workIcon1").hover(function(){
+    $("#work1").show();
+  })
+  $("#workIcon2").hover(function(){
+    $("#work2").show();
+  })
+  $("#workIcon3").hover(function(){
+    $("#work3").show();
+  })
+  $("#workIcon4").hover(function(){
+    $("#work4").show();
+  })
+  $("#workIcon5").hover(function(){
+    $("#work5").show();
+  })
+  $("#workIcon6").hover(function(){
+    $("#work6").show();
+  })
+  $("#workIcon7").hover(function(){
+    $("#work7").show();
+  })
+  $("#workIcon8").hover(function(){
+    $("#work8").show();
+  })
+});
