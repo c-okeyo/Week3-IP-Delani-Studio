1f443ac7016ce4c7d294db2ad2b1aa734f338671
c-okeyo <chrisokello26@gmail.com>
2022-11-13 19:11:55 +0300

    Formatting added'

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,18 @@
 $(document).ready(function () {
   $("#design-icon").click(function () {
-    $('#product-management').css('display', 'none');
-    $('#development').css('display', 'none');
+    $("#product-management").css("display", "none");
+    $("#development").css("display", "none");
     $("#design").show();
-
   });
   $("#dev-icon").click(function () {
     $("#development").show();
-    $('#design').css('display', 'none');
-    $('#product-management').css('display', 'none');
-    
+    $("#design").css("display", "none");
+    $("#product-management").css("display", "none");
   });
   $("#product-icon").click(function () {
     $("#product-management").show();
-    $('#development').css('display', 'none');
-    $('#design').css('display', 'none');
+    $("#development").css("display", "none");
+    $("#design").css("display", "none");
 
   });
 });
