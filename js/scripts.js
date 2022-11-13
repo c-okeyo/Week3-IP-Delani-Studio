7aa93acc3dc454c06a127d4b1bc3f8fa392e6a8b
c-okeyo <chrisokello26@gmail.com>
2022-11-13 18:51:50 +0300

    Toggle functionality added to what-we-do

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,20 @@
 $(document).ready(function () {
   $("#design-icon").click(function () {
+    $('#product-management').css('display', 'none');
+    $('#development').css('display', 'none');
     $("#design").show();
+
   });
   $("#dev-icon").click(function () {
     $("#development").show();
+    $('#design').css('display', 'none');
+    $('#product-management').css('display', 'none');
+    
   });
   $("#product-icon").click(function () {
     $("#product-management").show();
+    $('#development').css('display', 'none');
+    $('#design').css('display', 'none');
+
   });
 });
