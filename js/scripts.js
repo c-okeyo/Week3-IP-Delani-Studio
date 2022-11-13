08f1c26c53b5fe215bd9b62d287e5ba25cc8c6bd
c-okeyo <chrisokello26@gmail.com>
2022-11-13 19:47:04 +0300

    Portfolio hover functionality added

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,17 @@
 $(document).ready(function () {
-  $("#design-icon").click(function () {
+  $("#design-icon").click(function(){
     $("#product-management").css("display", "none");
     $("#development").css("display", "none");
     $("#design").show();
   });
-  $("#dev-icon").click(function () {
+  $("#dev-icon").click(function(){
     $("#development").show();
     $("#design").css("display", "none");
     $("#product-management").css("display", "none");
   });
-  $("#product-icon").click(function () {
+  $("#product-icon").click(function(){
     $("#product-management").show();
     $("#development").css("display", "none");
     $("#design").css("display", "none");
-
   });
 });
